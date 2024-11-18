1965bb249db9978d5edf7d95f555fd6a0e646e3e
Sergey Bespalov <sergey.bespalov@protonmail.com>
2024-11-18 11:23:15 +0300

    Activity 1.01 – defining and printing

---
+++ Chapter1/Activity01.01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Bob"
+	family := "Smith"
+	age := 34
+	allergy := false
+	fmt.Println(name)
+	fmt.Println(family)
+	fmt.Println(age)
+	fmt.Println(allergy)
+}
