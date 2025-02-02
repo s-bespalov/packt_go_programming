4c0c6c5136328a5bfedde1640248cfbff6df9de2
Sergey Bespalov <sergey.bespalov@protonmail.com>
2025-02-02 22:06:23 +0300

    Activity 3.02 - refactoring, fix bugs

--- Chapter3/Activity03.02/main.go
+++ Chapter3/Activity03.02/main.go
@@ -1,67 +1,72 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 )
 
+var (
+	goodCreditScore  int     = 450
+	goodRate         float64 = 0.15
+	baseRate         float64 = 0.2
+	goodMonthlyLimit float64 = 0.2
+	baseMonthlyLimit float64 = 0.1
+)
+
+func scanInput(prompt string, dest interface{}) {
+	fmt.Println(prompt)
+	_, err := fmt.Scan(dest)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func calculateCredit(income, loanAmount float64, loanTerm, creditScore int) (rate, monthly, totalCost float64, approved bool, err error) {
+	if loanTerm%12 != 0 {
+		err = errors.New("term of the loan should be divisible by 12 month")
+		return
+	}
+	isGood := creditScore >= goodCreditScore
+	if isGood {
+		rate = goodRate
+	} else {
+		rate = baseRate
+	}
+	years := loanTerm / 12
+	totalCost = (loanAmount * rate) * float64(years)
+	monthly = (loanAmount + totalCost) / float64(loanTerm)
+	if isGood {
+		approved = (goodMonthlyLimit * income) >= monthly
+	} else {
+		approved = (baseMonthlyLimit * income) >= monthly
+	}
+	return
+}
+
 func main() {
 	var name string
-	fmt.Println("Inpunt name")
-	_, err := fmt.Scan(&name)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var creditScore int
-	fmt.Println("Inpunt credit score")
-	_, err = fmt.Scan(&creditScore)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var income float64
-	fmt.Println("Inpunt income")
-	_, err = fmt.Scan(&income)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var loanAmount float64
-	fmt.Println("Input loan Amount")
-	_, err = fmt.Scan(&loanAmount)
-	if err != nil {
-		log.Fatal(err)
-	}
 	var loanTerm int
-	fmt.Println("Inpunt loan term")
-	_, err = fmt.Scan(&loanTerm)
+	scanInput("Input name", &name)
+	scanInput("Input credit score", &creditScore)
+	scanInput("Input monthly income", &income)
+	scanInput("loan Amount", &loanAmount)
+	scanInput("Input loan term", &loanTerm)
+	rate, monthly, totalCost, approved, err := calculateCredit(income, loanAmount, loanTerm, creditScore)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if loanTerm%12 != 0 {
-		log.Fatal("term of the loan should be divisile by 12 month")
-	}
-	isGood := creditScore > 450
-	var rate float64
-	if isGood {
-		rate = 0.15
-	} else {
-		rate = 0.2
-	}
-	var total float64 = (rate * (float64(loanTerm / 12)) * loanAmount) + loanAmount
-	var monthly float64 = total / 12
-	var approved bool
-	if isGood {
-		approved = monthly <= income/12*0.2
-	} else {
-		approved = monthly <= income/12*0.15
-	}
-	fmt.Println()
-	fmt.Println("Application:", name)
+	fmt.Println("\nApplication:", name)
 	fmt.Println("----------------------")
 	fmt.Println("Credit Score:", creditScore)
+	fmt.Println("income:", income)
 	fmt.Println("Loan Amount:", loanAmount)
 	fmt.Println("Loan Term:", loanTerm)
 	fmt.Println("Monthly Payment:", monthly)
 	fmt.Println("Rate:", int(rate*100), "%")
-	fmt.Println("Total Cost:", total-loanAmount)
+	fmt.Println("Total Cost:", totalCost)
 	fmt.Println("Approved:", approved)
 }
