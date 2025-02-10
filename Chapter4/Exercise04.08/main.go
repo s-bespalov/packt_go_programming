478c47e57f5841a9aeb0722fcdb6e230333d8173
Sergey Bespalov <sergey.bespalov@protonmail.com>
2025-02-11 01:41:23 +0300

    Exercise 4.08 - Working with slices

---
+++ Chapter4/Exercise04.08/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func getPassedArgs(minArgs int) []string {
+	if len(os.Args) < minArgs {
+		fmt.Printf("At least %v arguments are needed\n", minArgs)
+		os.Exit(1)
+	}
+	var args []string
+	for i := 1; i < len(os.Args); i++ {
+		args = append(args, os.Args[i])
+	}
+	return args
+}
+
+func findLongest(args []string) string {
+	var longest string
+	for i := 0; i < len(args); i++ {
+		if len(args[i]) > len(longest) {
+			longest = args[i]
+		}
+	}
+	return longest
+}
+
+func main() {
+	if longets := findLongest(getPassedArgs(3)); len(longets) > 0 {
+		fmt.Println("The longest word passed was:", longets)
+	} else {
+		fmt.Println("There was an error")
+		os.Exit(1)
+	}
+}
