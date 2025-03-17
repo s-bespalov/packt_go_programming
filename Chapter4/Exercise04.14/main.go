2916bf07a69779d84e4982614c9e8e75ad155d3a
Sergei Bespalov <sergey.bespalov@protonmail.com>
2025-03-17 10:59:42 +0300

    Exercise 4.14 – Reading from a map

---
+++ Chapter4/Exercise04.14/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func getUsers() map[string]string {
+	users := map[string]string{
+		"305": "Sue",
+		"204": "Bob",
+		"631": "Jake",
+		"073": "Tracey",
+	}
+	return users
+}
+
+func getUser(id string) (string, bool) {
+	users := getUsers()
+	user, exist := users[id]
+	return user, exist
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("User ID not passed")
+		os.Exit(1)
+	}
+	userId := os.Args[1]
+	name, exist := getUser(userId)
+	if !exist {
+		fmt.Printf("Passed user ID (%v) not found\n", userId)
+		for key, value := range getUsers() {
+			fmt.Println("  ID: ", key, "Name:", value)
+		}
+		os.Exit(1)
+	}
+	fmt.Println(name)
+}
