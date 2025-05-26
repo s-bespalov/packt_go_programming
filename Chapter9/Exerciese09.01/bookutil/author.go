cf712c78a1a69afa768de582aa9c8206d43693e9
Sergei Bespalov <sergey.bespalov@protonmail.com>
2025-05-26 08:40:58 +0300

    Exercise 09.02 - p1

---
+++ Chapter9/Exerciese09.01/bookutil/author.go
@@ -0,0 +1,26 @@
+package author
+
+import "fmt"
+
+type Author struct {
+	Name    string
+	Contact string
+}
+
+func NewAuthor(name, contact string) *Author {
+	return &Author{Name: name, Contact: contact}
+}
+
+func (a *Author) WriteChapter(chapterTitle string, content string) {
+	fmt.Printf("Author %s is writing a chapter titled a.Name, chapterTitle)'%s'\n", a.Name, chapterTitle)
+	fmt.Println(content)
+}
+
+func (a *Author) ReviewChapter(chapterTitle string, content string) {
+	fmt.Printf("Author %s is reviewing a chapter titled '%s'\n", a.Name, chapterTitle)
+	fmt.Println(content)
+}
+
+func (a *Author) FinalizeChapter(chapterTitle string) {
+	fmt.Printf("Author %s has finalized the chapter titled'%s'.\n", a.Name, chapterTitle)
+}
---
+++ Chapter9/Exerciese09.01/bookutil/go.mod
@@ -0,0 +1,3 @@
+module github.com/s-bespalov/packt_go_programming/Chapter9/Exercise09.01/bookutil
+
+go 1.24.2
