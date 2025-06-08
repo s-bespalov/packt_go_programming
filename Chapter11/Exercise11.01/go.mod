5866708517fbf70c906af1ee7549bd6ef28885ed
Sergei Bespalov <sergey.bespalov@protonmail.com>
2025-06-08 12:16:30 +0300

    Exercise 11.01 – Working with fmt.Println

---
+++ Chapter11/Exercise11.01/go.mod
@@ -0,0 +1,3 @@
+module github.com/s-bespalov/packt_go_programming/Chapter11/Exercise11.01
+
+go 1.24.3
---
+++ Chapter11/Exercise11.01/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fname := "Edward"
+	lname := "Sicissorhands"
+	fmt.Println("Hello:", fname, lname)
+	fmt.Println("Next line")
+}
