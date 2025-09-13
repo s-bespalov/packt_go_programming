b290307f08f5f6cf20dc9e85123172194569c5d3
Sergei Bespalov <sergey.bespalov@protonmail.com>
2025-09-14 02:46:11 +0300

    Exercise 14.02 – backing up files

---
+++ Chapter14/Exercise14.02/go.mod
@@ -0,0 +1,3 @@
+module github.com/s-bespalov/packt_go_programming/Chapter14/Exercise14.02
+
+go 1.25.0
---
+++ Chapter14/Exercise14.02/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+)
+
+var ErrorWorkingFileNotFount = errors.New("the working file is not found")
+
+func CreateBackup(working, backup string) error {
+	_, err := os.Stat(working)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return ErrorWorkingFileNotFount
+		}
+		return err
+	}
+	workfile, err := os.Open(working)
+	if err != nil {
+		return err
+	}
+	defer workfile.Close()
+	content, err := io.ReadAll(workfile)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(backup, content, 0644)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return nil
+}
+
+func AddNotes(workingFile, notes string) error {
+	notes += "\n"
+	f, err := os.OpenFile(workingFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	if _, err := f.Write([]byte(notes)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	backupFile := "backupFile.txt"
+	workingFile := "note.txt"
+	data := "note"
+	err := CreateBackup(workingFile, backupFile)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	for i := 1; i <= 10; i++ {
+		note := data + " " + strconv.Itoa(i)
+		err := AddNotes(workingFile, note)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	}
+}
