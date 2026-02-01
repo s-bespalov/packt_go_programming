303ec499d6f27a27bfaabd1a7b310c2bd22da8cd
Sergei Bespalov <sergey.bespalov@protonmail.com>
2026-02-01 23:20:03 +0300

    Exercise 18.09 – notifying when the computation has finished

---
+++ Chapter18/Exercise18.09/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "log"
+
+func readThem(in, out chan string) {
+	for i := range in {
+		log.Println(i)
+	}
+	out <- "done"
+}
+
+func main() {
+	log.SetFlags(0) // disable logs
+	in, out := make(chan string), make(chan string)
+	go readThem(in, out)
+	strs := []string{"a", "b", "c", "d", "e", "f"}
+	for _, s := range strs {
+		in <- s
+	}
+	close(in)
+	<-out
+}
